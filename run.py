f79b989f9df413a5d6d460342dc77e00d492b5bb
hormalno <ivanov.yasen@gmail.com>
2025-05-06 20:57:57 +0300

    adding run.py

--- run.py
+++ run.py
@@ -2,9 +2,15 @@ import os
 import sys
 import streamlit.web.cli as stcli
 
-# Set working directory to the project root (important for config.toml)
+# Set working directory to project root
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "src/main.py"]
+if __name__ == "__main__":
+    sys.argv = [
+        "streamlit",
+        "run",
+        "src/main.py",
+        "--server.port=$PORT",  # Port Azure assigns
+        "--server.address=0.0.0.0"  # Accessible to outside world
+    ]
     sys.exit(stcli.main())
